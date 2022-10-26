58814c9370c5a102e128fef879548107794e2586
Axel Godoy <115379527+Gargodax@users.noreply.github.com>
2022-10-26 07:40:45 -0300

    Commit sin cambios significativos.
    Se realizó ordenamiento del código.

--- script.js
+++ script.js
@@ -11,17 +11,13 @@ function traerProvincia() {
 
         .then(data => data.json())
         .then(data => {
-
             let opt = '<option value="Elige una provincia">Elige una provincia</option>';
-
             data.provincias.forEach(element => opt += `<option value=${element.nombre}>${element.nombre}</option>)`);
-
             selectProvincias.innerHTML = opt;
-
         })
+    
         .catch(error => {
             let message = error.statusText || "<b>-Ocurrió un error-</b></br>";
-
             selectProvincias.nextElementSibling.innerHTML = `${message}`;
         })
 }
@@ -32,20 +28,17 @@ function elegirMunicipio(provincias) {
     fetch(`https://apis.datos.gob.ar/georef/api/municipios?provincia=${provincias}&max=1000`)
         .then(data => data.json())
         .then(data => {
-
             let opt = '<option value="Elige un municipio">Elige un municipio</option>';
-
             data.municipios.forEach(element => opt += `<option value=${element.id}>${element.nombre}</option>)`);
-
             selectMunicipio.innerHTML = opt;
-
         })
+    
         .catch(error => {
             let message = error.statusText || "<b>-Ocurrió un error-</b></br>";
-
             selectMunicipio.nextElementSibling.innerHTML = `${message}`;
         })
 }
+
 selectProvincias.addEventListener("change", e => {
     elegirMunicipio(e.target.value);
     console.log(e.target.value)
@@ -57,20 +50,17 @@ function elegirLocalidad(municipios) {
     fetch(`https://apis.datos.gob.ar/georef/api/localidades?municipio=${municipios}&max=1000`)
         .then(data => data.json())
         .then(data => {
-
             let opt = '<option value="Elige una localidad">Elige una localidad</option>';
-
             data.localidades.forEach(element => opt += `<option value=${element.id}>${element.nombre}</option>)`);
-
             selectLocalidad.innerHTML = opt;
-
         })
+    
         .catch(error => {
             let message = error.statusText || "<b>-Ocurrió un error-</b></br>";
-
             selectLocalidad.nextElementSibling.innerHTML = `${message}`;
         })
 }
+
 selectMunicipio.addEventListener("change", e => {
     elegirLocalidad(e.target.value);
     console.log(e.target.value)
